18f6ef194690f936ada733c1c6ac90ea122d40d7
olashina201 <olashina201@gmail.com>
2024-02-06 15:45:36 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 // import { useState } from 'react'
 import "./App.css";
 import "./index.css";
-import Board from "./components/Board";
-// import Leaderboard from "./components/LeaderBoard";
-import { LeaderboardData } from "./data/data";
+import { sampleData } from "./data/data";
+import Leaderboard from "./components/LeaderBoard";
 
 function App() {
-  const sampleData = [
-    { id: 1, image: "User1", address: 'jnjhfvjefbjdbjbjbdjhbdjhbehbfjhbejh', score: 100 },
-    { id: 2, image: "User2", address: 'jnjhfvjefbjdbjbjbdjhbdjhbehbfjhbejh', score: 90 },
-  ];
-  return <Board LeaderboardData={LeaderboardData} />;
-  // return <Leaderboard leaderboardData={sampleData} />;
+  return <Leaderboard leaderboardData={sampleData} />;
 }
 
 export default App;
--- src/data/data.ts
+++ src/data/data.ts
@@ -42,3 +42,9 @@ export const LeaderboardData = [
     dt: "2022-01-21",
   },
 ];
+
+
+export const sampleData = [
+  { id: 1, image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60", address: 'jnjhfvjefbjdbjbjbdjhbdjhbehbfjhbejh', score: 100 },
+  { id: 2, image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60", address: 'jnjhfvjefbjdbjbjbdjhbdjhbehbfjhbejh', score: 90 },
+];
